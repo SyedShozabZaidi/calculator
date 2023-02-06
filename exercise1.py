7438f8fa2015d5d97ca72b529a329396dfb1f65c
SyedShozabZaidi <shozthegreat@gmail.com>
2023-02-07 03:09:57 +0500

    first commit

---
+++ exercise1.py
@@ -0,0 +1,6 @@
+u = 8
+a = 33
+print("the number of", u,  "+",  33, "is:", u + a) 
+print("the number of", u,  "-",  33, "is:", u - a) 
+print("the number of", u,  "/",  33, "is:", u / a) 
+print("the number of", u,  "*",  33, "is:", u * a) 
